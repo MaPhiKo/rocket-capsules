30a38ab95e4ff8c2745a349c42d8b0e52046f170
MaPhiKo <koch.marcphilipp@gmail.com>
2024-05-13 13:47:23 +0200

    Update: Filterfunktion implementiert um Kapseln je nach Status ein- bzw. auszublenden

--- src/components/CapsulesList.vue
+++ src/components/CapsulesList.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
-	import { ref, onMounted } from 'vue';
+	import { ref, onMounted, reactive, computed } from 'vue';
 	import CapsuleCard from '@/components/CapsuleCard.vue';
-	import type { Capsule } from '@/types';
+	import type { Capsule, CapsuleStatus } from '@/types';
 	import { useToast } from 'vue-toast-notification';
 	import 'vue-toast-notification/dist/theme-sugar.css';
 
 	const $toast = useToast();
 	const capsules = ref<Capsule[] | null>(null);
+	const capsulesVisibility = reactive({
+		unknown: true,
+		active: true,
+		retired: true,
+		destroyed: true,
+	});
 
+	const visibleCapsules = computed(() => {
+		return capsules.value?.filter((capsule) => {
+			return capsulesVisibility[capsule.status];
+		});
+	});
+
+	const toggleVisibility = (status: CapsuleStatus) => {
+		capsulesVisibility[status] = !capsulesVisibility[status];
+	};
 	const launchCapsule = (serial: string) => {
 		$toast.success(`Raumkapsel ${serial} startet`);
 	};
-
 	onMounted(async () => {
 		const response = await fetch('https://api.spacexdata.com/v4/capsules');
 		const data = await response.json();
@@ -21,15 +35,27 @@
 	});
 </script>
 <template>
-	<div class="capsules">
-		<CapsuleCard
-			v-if="capsules"
-			@launch="launchCapsule(capsule.serial)"
-			v-for="capsule of capsules"
-			:key="capsule.id"
-			:capsule
-		/>
-		<div v-else>loading..</div>
+	<div>
+		<div class="statusToggles">
+			<button
+				type="button"
+				v-for="(value, status) of capsulesVisibility"
+				:key="status"
+				@click="toggleVisibility(status)"
+			>
+				{{ status }}
+			</button>
+		</div>
+		<div class="capsules">
+			<CapsuleCard
+				v-if="visibleCapsules"
+				@launch="launchCapsule(capsule.serial)"
+				v-for="capsule of visibleCapsules"
+				:key="capsule.id"
+				:capsule
+			/>
+			<div v-else>loading..</div>
+		</div>
 	</div>
 </template>
 <style scoped>
--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,9 @@ export type Capsule = {
 	launches: string[];
 	reuse_count: number;
 	serial: string;
-	status: 'unknown' | 'active' | 'retired' | 'destroyed';
+	status: CapsuleStatus;
 	type: 'Dragon 1.0' | 'Dragon 1.1' | 'Dragon 2.0';
 	water_landings: number;
 };
+
+export type CapsuleStatus = 'unknown' | 'active' | 'retired' | 'destroyed';
